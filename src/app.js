15349ad2a0f188cd4a8a35054b430b469f4cffb1
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-03-23 14:19:11 -0700

    error handling in apis

--- src/app.js
+++ src/app.js
@@ -6,18 +6,20 @@ const { adminAuth, userAuth } = require("../middlewares/auth");
 const app = express();
 
 
-app.use("/admin",adminAuth);
-app.use("/user",userAuth);
+app.get("/getUserData",(req,res)=>{
+    // try{
+        throw new Error ("abc");
+        res.send("User Data Sent");
+    // }
+    // catch(err){
+    //     res.status(500).send("Something went Wrong from api");
+    // }    
+});
 
-app.get("/user",(req,res)=>{
-    res.send("Hi user");    
-})
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All Data Sent");
-})
-
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted USer");    
-})
+//catches errors of all apis for which try catch not used explicitly
+app.use("/",(err,req, res, next)=>{
+    if(err) res.status(500).send("Something went Wrong from middleware");
+});
+//keep it at the end else it wont match after api starts executing 
 
 app.listen(3000,()=>{console.log("server listening on 3000")});
