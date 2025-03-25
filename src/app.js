b93fe36334cd47fe68c7df47257930d3ea0a0ed2
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-03-25 13:17:27 -0700

    diving into the apis

--- src/app.js
+++ src/app.js
@@ -17,7 +17,60 @@ app.post("/signup",async (req, res) => {
     } catch(err) {
         res.status(400).send("Error Saving User", err.message)
     }
-})
+});
+
+//Get user by emailId
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.findOne({ emailId: userEmail });
+        if(!user) return res.status(404).send("User not found");
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("Something went wrong.")
+    }
+});
+
+//Feed API - fetch all users
+app.get("/feed", async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong.")
+    }
+});
+
+//Delete API
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({ _id : userId });
+        if(!user) return res.status(404).send("User not found");
+        res.send("User deleted sucessfully");
+    } catch (err) {
+        res.status(400).send("Something went wrong.")
+    }
+});
+
+
+//Update data of the user
+app.patch("/user", async (req,res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate({ _id : userId }, data, {
+            returnDocument:"after"
+        });//any other data apart from scheema is ignored
+        console.log("🚀 ~ app.patch ~ user:", user)
+        if(!user) return res.status(404).send("User not found");
+        res.send("User updated sucessfully");
+    } catch (err) {
+        res.status(400).send("Something went wrong.")
+    }
+});
+
 connectDB()
 .then(
     ()=>{
