c4494f15ec31b80ea862f2ea902f6fdeb8e60809
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-03-25 12:02:36 -0700

    dynamic signup api

--- src/app.js
+++ src/app.js
@@ -3,17 +3,15 @@ console.log("Starting a new Project");
 const express = require("express");
 const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
+
 const app = express();
 
+app.use(express.json());//processing json data middle ware and no route means applicable for all app routes
+
 app.post("/signup",async (req, res) => {
-    const userObj = {
-        firstName:"Nauman",
-        lastName:"Ahmed",
-        emailId:"NA@NA.com",
-        password:"NA123",
-    };
-    try {
-        const user = new User(userObj);
+    
+    try { 
+        const user = new User(req.body);
         await user.save();
         res.send("User saved successfully");
     } catch(err) {
