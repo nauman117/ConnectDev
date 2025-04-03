fc634c173fcdf5e4e52d95d42c7be9dc6ee187f8
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-04-03 06:40:56 -0400

    query optimizations and compound indexes

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -24,13 +24,15 @@ const connectionRequestSchema = new mongoose.Schema(
     }
 );
 
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
 connectionRequestSchema.pre("save", function () {
     const connectionRequest = this;
     if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
         throw new Error("cannot send connection request to yourself");
     }
     next();
-})
+});
 
 const ConnectionRequest = new mongoose.model("ConnectionRequest", connectionRequestSchema);
 
