94a7a709b99e2729c6c21dd5037776e367abccfe
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-04-07 05:31:38 -0400

    feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -47,4 +47,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     }
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => { 
+    try {
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id},
+                {fromUserId: loggedInUser._id}
+            ]
+        })
+        .select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequest.forEach( req => {
+            hideUsersFromFeed.Add(req.fromUserId.toString());
+            hideUsersFromFeed.Add(req.toUserId.toString());
+        });
+        hideUsersFromFeed.Add(loggedInUser._id);
+
+        const data = User.find({
+            _id: { $nin: Array.from(hideUsersFromFeed) }
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip).limit(limit);
+        
+        if()
+        
+        res.json({ data: data });
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
 module.exports = userRouter;
